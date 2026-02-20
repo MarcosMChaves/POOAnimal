3e6157dda8a6876c55baf1ce38aa9b22fbacb207
Marcos Chaves <marcosdmchaves@gmail.com>
2026-02-20 12:51:44 -0300

    Adicionar arquivos de projeto.

---
+++ Animal.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+namespace POOAnimal
+{
+    internal class Animal
+    {
+        private string NomePopular;
+        private string Genero;
+        private string Especie;
+
+        public Animal(string nomePopular, string genero, string especie)
+        {
+            if (!(EhValido(nomePopular) && EhValido(genero) && EhValido(especie)))
+            {
+                throw new ArgumentException("Argumentos com conteúdo inválido!");
+            }
+            NomePopular = Sanitizar(nomePopular.ToLower());
+            Genero = Sanitizar(genero.Substring(0,1).ToUpper() + genero.Substring(1).ToLower());
+            Especie = Sanitizar(especie.ToLower());
+        }
+
+        private bool EhValido(string textToValidade)
+        {
+            if (String.IsNullOrWhiteSpace(textToValidade) || 
+                String.IsNullOrEmpty(textToValidade))
+            {
+                return false;                
+            }
+
+            return true;
+        }
+        private string Sanitizar(string textToSanitize)
+        {
+            string patternA = @"[!#$%¨&*()_+=`´^~{}:;><,|\/?'0-9]";
+            // Remove símbolos/dígitos da string
+            textToSanitize = Regex.Replace(textToSanitize, patternA, String.Empty);
+
+            string patternB = @"(^\W+)|(\W+$)"; 
+            // Remove símbolos do início e fim da string
+            return Regex.Replace(textToSanitize, patternB, String.Empty).Replace("spp","spp.").Trim();
+
+        }
+        public string GetNomePopular()
+        {
+            return NomePopular;
+        }
+        public string GetNomeCientifico()
+        {
+            return Genero + " " + Especie;
+        }
+
+        public string EmitirSom()
+        {
+            return "rrrrr..."; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+using POOAnimal;
+
+const string ITALICS = "\u001b[3m";
+const string RESET = "\u001b[0m";
+
+
+Animal gato = new Animal("Gato-Comum.1", "Fe%lis", "Catus32&# spp.");
+
+WriteLine($"Nome popular= {gato.GetNomePopular()}");
+Write($"Nome científico= '");
+ForegroundColor = ConsoleColor.Blue;
+Write($"{ITALICS}{gato.GetNomeCientifico()}{RESET}");
+ResetColor();
+WriteLine($"'");
+WriteLine($"o gato está bravo '{gato.EmitirSom()}'");
+
